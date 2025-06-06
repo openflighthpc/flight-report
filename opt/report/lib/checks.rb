00049d1a1e55720288540388e5d449f0fe6c076e
Stu Franks <stu@stuts.uk>
2025-06-06 09:44:34 +0100

    Add missing file

---
+++ opt/report/lib/checks.rb
@@ -0,0 +1,64 @@
+require 'gpgme'
+
+def check_privileged
+  return Config.privileged_check_users.include?(ENV['USER'])
+end
+
+def get_checks(include_privileged, password)
+  checks = []
+  regular_check_files = Dir["#{Config.checksdir}/*.sh"]
+  privileged_check_files = Dir["#{Config.checksdir}/*.sh.gpg"]
+
+  # Gather data for regular checks
+  regular_check_files.each do |checkfile|
+    checks.append(get_check_data(checkfile))
+  end
+
+  # Decrypt privileged scripts if we've been given a password
+  if include_privileged && password
+    # Headless decryption
+    @gpg = GPGME::Crypto.new
+    privileged_check_files.each do |priv_file|
+      checks.append(get_check_data(priv_file, true, password))
+    end
+  end
+
+  # Returns a hash of all available checks
+  return checks
+end
+
+def get_check_data(checkfile, encrypted=false, password=nil)
+  filename = File.basename(checkfile)
+  if encrypted
+    begin
+      content = @gpg.decrypt(File.read(checkfile), pinentry_mode: GPGME::PINENTRY_MODE_LOOPBACK, password: password).read
+    rescue GPGME::Error::BadPassphrase
+      puts "Failed to decrypt '#{filename}: Incorrect administrative password provided"
+      exit 1
+    end
+  else
+    content = File.read(checkfile)
+  end
+  
+  description = content.each_line.find {|line| line =~ /^# Description: / }&.sub('# Description: ','')&.strip()
+  if description.nil?
+    description = "No description provided"
+  end
+
+  out = {"name" => filename, "description" => description, "content" => content}
+  return out
+end
+
+def run_check(check_hash)
+  # Take the name, description & content hash, create tempfile, execute script
+  check_script = Tempfile.create
+  check_script.puts(check_hash['content'])
+  check_script.close
+
+  output = `bash #{check_script.path} 2>&1`
+
+  # Remove temp file
+  File.unlink(check_script.path)
+
+  return output
+end
